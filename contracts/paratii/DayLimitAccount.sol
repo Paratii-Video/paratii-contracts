c45c3ee8042f60b7c7fb1e839d62286a39eb2a5a
Jelle Gerbrandy <github@gerbrandy.com>
2017-03-27 17:42:38 +0200

    first version of daily limiter for mining contract

---
+++ contracts/paratii/DayLimitAccount.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.4;
+
+import "zeppelin/contracts/token/ERC20.sol";
+import "zeppelin/contracts/SafeMath.sol";
+
+/* This contract manages an account of an ERC20 Token
+ * it lock the tokens, and and releases not more than a given amount of tokens each day
+ * to a gven beneficary
+ */
+
+contract DayLimitAccount is SafeMath {
+	ERC20 public token;
+	uint public limit; // amount of tokens to release each day
+	uint public firstDay; // last day that tokens were released
+	uint public claimed; // amount of tokens that were already claimed
+	address public beneficary; // who gets the released tokens
+
+	function DayLimitAccount(ERC20 _token, address _beneficary, uint _limit) {
+		token = _token;
+		beneficary = _beneficary;
+		firstDay = today() - 1;
+		limit = _limit;
+	}	
+
+	// determines today's index.
+	function today() private constant returns (uint) {
+	  return now / 1 days;
+	}
+
+	// returns how much tokens are claimable
+	function claimable() constant returns (uint) {
+		uint total_claimable = safeMul(today() - firstDay, limit);
+		return (total_claimable - claimed);
+	}
+
+	// transfer all claimable tokens to the beneficary
+	function claim() {
+		uint _value = claimable();
+		claimed += _value; 
+		token.transfer(beneficary, _value);
+	}
+}
