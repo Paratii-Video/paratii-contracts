a89270d01a8d4af99882b0ec7a846c3b133cab84
Jelle Gerbrandy <github@gerbrandy.com>
2017-09-12 21:42:44 +0200

    first draft of a video registry

--- contracts/paratii/SendEther.sol
+++ contracts/paratii/SendEther.sol
@@ -1,26 +1,33 @@
 pragma solidity ^0.4.13;
 
 /**
- * @title SentEther
+ * @title SendEther
  * @dev A Contract that wraps the native transfer function and logs an event.
  */
 
 contract SendEther {
 
     event LogSendEther(
+      address from,
       address to,
       uint value,
       string description
     );
-    
+
     // If someone accidentally sends ether to this contract, revert;
     function () {
         revert();
     }
 
+    /**
+     * @dev Transfer ether to address _to, providing a description
+     * The only difference with sending ETH directly is that the action is logged
+     * @param _to recipient of the ETH
+     * @param _description  A description to log
+     */
     function transfer(address _to, string _description) payable returns(bool)  {
        _to.transfer(msg.value);
-       LogSendEther(_to, msg.value, _description);
+       LogSendEther(msg.sender, _to, msg.value, _description);
        return true;
     }
 
--- test/sendether.js
+++ test/sendether.js
@@ -5,8 +5,11 @@ contract('SendEther', function (accounts) {
   it('should send ether and log the event', async function () {
     let ctt = await SendEther.new()
     let value = 40 * (10 ** 18)
-    let tx = await ctt.transfer(accounts[2], 'a description', {value})
+    let description = 'I mean one thinks of it like being alive in a box, one keeps forgetting to take into account the fact that one is dead…which should make all the difference…shouldn\'t it?'
+    let tx = await ctt.transfer(accounts[2], description, {from: accounts[1], value})
+    assert.equal(getValueFromLogs(tx, 'from'), accounts[1])
     assert.equal(getValueFromLogs(tx, 'to'), accounts[2])
     assert.equal(getValueFromLogs(tx, 'value'), value)
+    assert.equal(getValueFromLogs(tx, 'description'), description)
   })
 })
