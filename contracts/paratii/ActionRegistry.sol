903272e7c34c7211c202889577c7829600912a64
Alex Eftimiades <alexeftimiades@gmail.com>
2017-10-18 00:44:21 +0700

    first base contract for logging arbitrary transactions passing initial tests

--- contracts/paratii/ActionRegistry.sol
+++ contracts/paratii/ActionRegistry.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.13;
 
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import './ParatiiAvatar.sol';
-import './ParatiiToken.sol';
+import './VideoRegistry.sol';
 import './ParatiiRegistry.sol';
 import "../debug/Debug.sol";
 
@@ -17,16 +17,28 @@ contract ActionRegistry is Ownable, Debug {
     using SafeMath for uint256;
 
     ParatiiRegistry public paratiiRegistry;
+    ParatiiAvatar public paratiiAvatar;
+    VideoRegistry public videoRegistry;
+    string public tag; // tag for this action
+
+    struct Receipt {
+        address recipient;
+        uint256 amount;
+    }
 
     event LogAction (
-      string videoId,
-      address user,
-      uint256 action,
-      string tag
+      string videoId, // the video this action applied to
+      address user, // the user who did this action
+      string tag, // tag used to encode class of action; e.g. "Like", "Tag", "BuyVideo", etc
+      string action // used to encode specifics of action, e.g. "Dislike", "Cats", "156 PTI", etc
     );
 
-    function ActionRegistry(ParatiiRegistry _paratiiRegistry) {
+    function ActionRegistry(ParatiiRegistry _paratiiRegistry,
+                            string _tag) {
       paratiiRegistry = _paratiiRegistry;
+      videoRegistry = VideoRegistry(paratiiRegistry.getContract('VideoRegistry'));
+      paratiiAvatar = ParatiiAvatar(paratiiRegistry.getContract('ParatiiAvatar'));
+      tag = _tag;
     }
 
     // If someone accidentally sends ether to this contract, revert;
@@ -35,49 +47,67 @@ contract ActionRegistry is Ownable, Debug {
     }
 
     /**
-     * @dev buyVideo msg.sender buys a video
+     * @dev TransferFunds, and log action
      * For the transaction to succeed, the buyer must have approved for the ParatiiAvatar to transfer
      * the sum to the owner and the redistribution pool.
-     * [TODO] The fact that the user has bought this video will be registred in the VideoRegistry.
-     * A successful transaction logs the LogBuyVideo event
      */
-    function Act(string videoId,
-                 string action_contract,
-                 string transfer_contract) public returns(bool) {
+    function transfer(string videoId, string action) public {
+        address actor = msg.sender;
+        Receipt[] memory receipts = getReceipts(actor, videoId, action);
+        uint256 amount;
+        uint256 paratiiPart;
+        uint256 paratiiFraction = redistributionPoolShare().div(10 ** 18);
+        uint256 paratiiTotal = 0;
+        address recipient;
+        Receipt memory receipt;
+        for (uint i=0; i < receipts.length; ++i) {
+            receipt = receipts[i];
+            amount = receipt.amount;
+            paratiiPart = amount.mul(paratiiFraction);
+            paratiiTotal += paratiiPart;
+            amount -= paratiiPart;
+            paratiiAvatar.transferFrom(actor, recipient, amount);
+        }
+        paratiiAvatar.transferFrom(actor, address(paratiiAvatar), paratiiTotal);
+        LogAction(videoId, actor, action, tag);
+    } 
 
-       address buyer = msg.sender;
+    /**
+     * @dev getReceipts returns a series of recipients and the amounts they should receive
+     * to be overridden by specific subcontracts
+     */
+    function getReceipts(address actor, string videoId, string action) internal returns (Receipt[]) {
 
-       // get the info about the video
-       VideoRegistry videoRegistry = VideoRegistry(paratiiRegistry.getContract('VideoRegistry'));
-       var (owner, price) = videoRegistry.getVideoInfo(videoId);
+        videoRegistry = VideoRegistry(paratiiRegistry.getContract('VideoRegistry'));
+        var (owner, price) = videoRegistry.getVideoInfo(videoId);
 
-       TransferContract transferContract = TransferContract(paratiiRegistry.getContract(transfer_contract));
+        Receipt receipt;
+        receipt.recipient = owner;
+        receipt.amount = price;
 
-       ActionContract actionContract = ActionContract(paratiiRegistry.getContract(action_contract));
-
-       // collect redistribution funds
-       uint256 paratiiPart = price.mul(redistributionPoolShare()).div(10 ** 18);
-       ParatiiAvatar paratiiAvatar = ParatiiAvatar(paratiiRegistry.getContract('ParatiiAvatar'));
-       paratiiAvatar.transferFrom(buyer, address(paratiiAvatar),  paratiiPart);
-
-       uint256 ownerPart = price.sub(paratiiPart);
-
-       transferContract.transferFrom(buyer, owner, ownerPart);
-       // paratiiAvatar.transferFrom(buyer, owner, ownerPart);
-       LogAction(videoId, buyer, price, 'buyVido');
-       return true;
+        Receipt[] memory receipts;
+        receipts[1] = receipt;
+        return receipts;
     }
 
     function redistributionPoolShare() internal constant returns(uint256) {
         // the "percentage" in precision 10**18
         return paratiiRegistry.getUint('VideoRedistributionPoolShare');
     }
-}
 
-contract TransferContract {
-
-}
-
-contract ActionContract {
- 
+    /**
+     * @dev Potentially useful method for converting strings to uints
+     * [TODO] check input for sanity
+     */
+    function stringToUint(string s) constant returns (uint result) {
+        bytes memory b = bytes(s);
+        uint i;
+        result = 0;
+        for (i = 0; i < b.length; i++) {
+            uint c = uint(b[i]);
+            if (c >= 48 && c <= 57) {
+                result = result * 10 + (c - 48);
+            }
+        }
+    }
 }
