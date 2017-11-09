3b3b994a0b6ead5f611c4ac3cc6ab7f96d801601
Alex Eftimiades <alexeftimiades@gmail.com>
2017-11-09 13:26:57 -0500

    Increase gas limit so contracts can be deployed

--- truffle.js
+++ truffle.js
@@ -5,17 +5,12 @@ require('babel-register')({
 })
 
 module.exports = {
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  },
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*', // Match any network id
+      gas: 4600000
     },
     test: {
       host: 'localhost',
