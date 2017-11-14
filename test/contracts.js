0ba7229a83052f954d3fa23f2bda50a2795ee345
Jelle Gerbrandy <github@gerbrandy.com>
2017-11-14 08:57:10 +0100

    copy from code from the player

---
+++ test/contracts.js
@@ -0,0 +1,6 @@
+import { getContract } from '../lib/contracts.js'
+
+contract('Contracts library', function (accounts) {
+  it('the getContract functions works as expected', async function () {
+  })
+})
