e0856c8f7a4745cb5d5d39b820a798ff869489bb
Jelle Gerbrandy <github@gerbrandy.com>
2017-11-17 21:35:33 +0100

    set gas on truffle.js

--- truffle.js
+++ truffle.js
@@ -15,17 +15,20 @@ module.exports = {
     test: {
       host: 'localhost',
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*', // Match any network id
+      gas: 4600000
     },
     paratii: {
       host: 'chain.paratii.video',
       port: 80,
-      network_id: '1' // Match any network id
+      network_id: '1', // Match any network id
+      gas: 4600000
     },
     paratii_local: {
       host: 'localhost',
       port: 8999,
-      network_id: '1' // Match any network id
+      network_id: '1', // Match any network id
+      gas: 4600000
     }
   }
 }
