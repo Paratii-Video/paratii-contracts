e2352d3ea3150702496e03bf351346ef6ced0b81
Geckoslair <enrico@piramid.studio>
2018-05-14 19:09:16 +0200

    new contract concept for distribution

--- contracts/paratii/PTIDistributor.sol
+++ contracts/paratii/PTIDistributor.sol
@@ -8,24 +8,23 @@ contract PTIDistributor is Ownable {
 
     Registry public registry;
 
-    event LogDistribute(address _toAddress, uint _amount, string _reason);
+    event LogDistribute(address _toAddress, uint _amount);
+    mapping(bytes32 => bool) public isUsed;
 
     function PTIDistributor(Registry _registry) public {
         owner = msg.sender;
         registry = _registry;
     }
 
-    modifier onlyOwnerOrAvatar() {
-        require((msg.sender == owner) || (msg.sender == registry.getContract('Avatar')));
-        _;
-    }
-
     function() public payable { }
 
-    function distribute(address _toAddress, uint _amount, string _reason) public onlyOwnerOrAvatar returns(bool) {
-        //reason: reason of the distribute,
+    function distribute(address _toAddress, uint256 _amount, bytes32 _signedNonce, uint8 _v, bytes32 _r, bytes32 _s) {
+
+      if(ecrecover(_signedNonce, _v, _r, _s) == owner && !isUsed[_signedNonce]) {
+        isUsed[_signedNonce] = true;
         ParatiiToken token = ParatiiToken(registry.getContract('ParatiiToken'));
         token.transfer(msg.sender, _amount);
-        LogDistribute(_toAddress, _amount, _reason);
+        LogDistribute(_toAddress, _amount);
+      }
     }
 }
--- test/distributor.js
+++ test/distributor.js
@@ -4,21 +4,53 @@ import {
   paratiiToken,
   getInfoFromLogs
 } from './utils.js'
+var ethUtil = require('ethereumjs-util')
+var uuidv1 = require('uuid/v1')
 
 contract('Distributor: ', function (accounts) {
   let address = accounts[2]
-  let amount = 1 * 10 ** 18
-  let reason = 'email'
+  let owner = accounts[0]
+  const amount = 1 * 10 ** 18
+  const nonce = uuidv1()
 
   before(async function () {
     await setupParatiiContracts()
-
     await paratiiToken.transfer(ptiDistributor.address, 1 * 10 ** 18)
   })
 
+  // this test is inspired from https://github.com/davidmichaelakers/ecrecover/ TY!
+
   it('the contract should work as expected', async function () {
-    // calculate the hash
-    let distribute = await ptiDistributor.distribute(address, amount, reason)
+    let messagetoSign = ethUtil.bufferToHex(Buffer.alloc(nonce.length, nonce))
+    let messagetoSend
+    let hashBuff
+    let msgHashBuff
+
+    switch (this.nodeVersion) {
+      default:
+        hashBuff = ethUtil.toBuffer(nonce)
+        msgHashBuff = ethUtil.hashPersonalMessage(hashBuff)
+        messagetoSend = ethUtil.bufferToHex(msgHashBuff)
+        break
+    }
+
+    var signature = await web3.eth.sign(owner, messagetoSign)
+
+    console.log(this.nodeVersion)
+    console.log(this.nodeVersion + ' sig: ' + signature)
+    console.log(this.nodeVersion + ' msg2sign: ' + messagetoSign)
+    console.log(this.nodeVersion + ' msg2send: ' + messagetoSend)
+    console.log()
+    console.log()
+
+    const signatureData = ethUtil.fromRpcSig(signature)
+    let v = ethUtil.bufferToHex(signatureData.v)
+    let r = ethUtil.bufferToHex(signatureData.r)
+    let s = ethUtil.bufferToHex(signatureData.s)
+
+    let distribute = await ptiDistributor.distribute(address, amount, messagetoSend, v, r, s)
+    console.log(distribute.logs)
+    console.log(owner)
     assert.equal(getInfoFromLogs(distribute, '_toAddress', 'LogDistribute'), accounts[2])
   })
 })
