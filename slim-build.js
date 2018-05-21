8252fc6f1dab6b671f410da2ee97a21115b7e4e0
LorenzoAlluminio <alluminio.lorenzo@gmail.com>
2018-05-21 17:40:27 +0200

    reduced bundle size by modifying contract's json with a script

---
+++ slim-build.js
@@ -0,0 +1,22 @@
+const folder = './build/contracts'
+const fs = require('fs')
+
+// read contracts folder
+const files = fs.readdirSync(folder)
+
+files.forEach((file) => {
+  const path = `${folder}/${file}`
+  // read original file
+  const json = require(path)
+  // pick just the thing we want
+  const obj = {
+    contractName: json.contractName,
+    abi: json.abi,
+    bytecode: json.bytecode,
+    deployedBytecode: json.deployedBytecode
+  }
+  // overwrites original file with slim file
+  fs.writeFile(`./build/contracts/${file}`, JSON.stringify(obj), (err) => {
+    if (err) throw err
+  })
+})
